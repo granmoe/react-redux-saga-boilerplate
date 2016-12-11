4e6a1ea7368d29f3e137be7ec3052a222dcd396d
Matt Granmoe <mattgranmoe@gmail.com>
2016-12-10 21:23:38 -0600

    forgot to commit new files

---
+++ src/components/counter.jsx
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+import { requestIncrement } from 'ducks/count'
+import 'components/counter.less'
+
+class Counter extends Component {
+  static propTypes = {
+    count: PropTypes.number.isRequired,
+    requestIncrement: PropTypes.func.isRequired
+  }
+
+  render () {
+    const { count, requestIncrement } = this.props
+
+    return (
+      <div className="counter">
+        <button onClick={ requestIncrement } type="button">increment async</button>
+        <p className="counter__count"> Count: { count }</p>
+      </div>
+    )
+  }
+}
+
+export default connect(state => {
+  return {
+    count: state.get('count')
+  }
+}, ({
+  requestIncrement
+}))(Counter)
